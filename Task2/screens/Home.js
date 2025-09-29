6e1a64809609018d6becb49605f24566b34d836f
lubdhie <lubdhie.pca@gmail.com>
2025-09-29 08:55:59 +0530

    final

--- Task2/screens/Home.js
+++ Task2/screens/Home.js
@@ -1,28 +1,56 @@
-import React from 'react';
-import { View, Text, StyleSheet,Button } from 'react-native';
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
-export default function Home({navigation}) {
+export default function Home({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Welcome to the Home Screen!</Text>
-    <Button title="Signup / Update Profile" onPress={() => navigation.navigate('Signup')} />
-    <View style={{ height: 10 }} />
-    <Button title="Go to Profile" onPress={() => navigation.navigate('Profile')} />
+      <Text style={styles.header}>Welcome</Text>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("Signup")}
+      >
+        <Text style={styles.buttonText}>Signup / Login</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.button, styles.secondaryButton]}
+        onPress={() => navigation.navigate("Profile")}
+      >
+        <Text style={styles.buttonText}>Go to Profile</Text>
+      </TouchableOpacity>
     </View>
-    );
-    }
-    
-    
-    const styles = StyleSheet.create({
-    container: { flex: 1, alignItems: 'center', justifyContent: 'center', padding: 16 },
-    title: { fontSize: 24, marginBottom: 20 },
-    });
-    
-    const SCREENS={
-        HOME : 'HOME',
-        PROFILE :'PROFILE',
-        SIGNUP : 'SIGNUP',
-        EDIT : 'EDIT',
-    };
-    
-    // export default SCREENS;
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#f9f9f9",
+    justifyContent: "center", // thoda sa vertical center
+  },
+  header: {
+    fontSize: 26,
+    fontWeight: "700",
+    marginBottom: 20,
+    color: "#333",
+    textAlign: "center", // text ko centre kiya
+  },
+  button: {
+    backgroundColor: "#007AFF",
+    padding: 14,
+    borderRadius: 8,
+    alignItems: "center",
+    marginBottom: 12,
+  },
+  secondaryButton: {backgroundColor: "#007AFF"},
+  buttonText: { color: "#fff", fontWeight: "600", fontSize: 15 },
+});
+
+const SCREENS = {
+  HOME: "HOME",
+  PROFILE: "PROFILE",
+  SIGNUP: "SIGNUP",
+  EDIT: "EDIT",
+};
